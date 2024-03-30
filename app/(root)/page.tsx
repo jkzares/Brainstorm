fd343a6bb17a05b0d2919f132ff82c8f9e6de525
jkzares <164260157+jkzares@users.noreply.github.com>
2024-03-29 21:48:33 -0400

    Add files via upload

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Editor from "@/components/sections/Editor";
+import Hero from "@/components/sections/Hero";
+
+export default function Home() {
+  return (
+    <main className="container">
+      <Hero />
+      <Editor />
+    </main>
+  )
+}
